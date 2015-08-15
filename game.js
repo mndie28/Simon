41edbc7427b1fddc903647583459058b87958d7d
John Godsey <johngodsey@gmail.com>
2015-08-15 15:34:29 -0400

    Refactored sound and added an intro.

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 var ac = new AudioContext();
 var tempo = 120;
+var octave = 3;
 
 var blueNote = new Note('E4 q');
 var redNote = new Note('A4 q');
@@ -17,6 +18,18 @@ newMove = function () {
     moves.push(Math.floor(Math.random() * 4) + 1);
 }
 
+function simonIntroduction()
+{
+    var sequence = new Sequence(ac, tempo, ['E4 q', 'A4 q', 'E3 q', 'C#4 q']);
+    sequence.loop = false;
+    sequence.play();
+    
+    playNote('E','3','w');
+    playNote('A','3','w');
+    playNote('E','2','w');
+    playNote('C#','3','w');        
+}
+
 presentMoves = function () {
     var i = 0;
     var interval = setInterval(function () {
@@ -26,27 +39,19 @@ presentMoves = function () {
         yellow.className = "tile";
             switch (moves[i]) {
                 case 1:
-                    var sequence = new Sequence(ac, tempo, ['E4 q']);
-                    sequence.loop = false;
-                    sequence.play();
+                    playNote('E',octave,'q');
                     blue.className = blue.className + " lit";
                     break;
                 case 2:
-                    var sequence = new Sequence(ac, tempo, ['A4 q']);
-                    sequence.loop = false;
-                    sequence.play();
+                    playNote('A',octave,'q');
                     red.className = red.className + " lit";
                     break;
                 case 3:
-                    var sequence = new Sequence(ac, tempo, ['E3 q']);
-                    sequence.loop = false;
-                    sequence.play();
+                    playNote('E',octave,'q');
                     green.className = green.className + " lit";
                     break;
                 case 4:
-                    var sequence = new Sequence(ac, tempo, ['C#4 q']);
-                    sequence.loop = false;
-                    sequence.play();
+                    playNote('C#',octave,'q');
                     yellow.className = yellow.className + " lit";
                     break;
             }
@@ -58,7 +63,12 @@ presentMoves = function () {
 
 }
 
-
+function playNote(note, octave, length)
+{
+    var sequence = new Sequence(ac, tempo, [note+octave+" "+length]);
+    sequence.loop = false;
+    sequence.play();
+}
 
 newMove();
 newMove();
@@ -71,3 +81,4 @@ newMove();
 newMove();
 newMove();
 presentMoves();
+//simonIntroduction();
