d8bbc4e1accd01733171cf7c4078fa2a73daf3d6
John Godsey <johngodsey@gmail.com>
2015-08-16 20:26:53 -0400

    Formatted the overhaul.

--- gameoverhaul.js
+++ gameoverhaul.js
@@ -2,73 +2,82 @@ var ac = new AudioContext();
 var tempo = 120;
 
 var simon = {
- health: 100,
+    health: 100,
 
- octave: 3,
- moves: [],
- health: 100,
-    
- blue: document.getElementById("blue"),
- red: document.getElementById("red"),
- green: document.getElementById("green"),
- yellow: document.getElementById("yellow"),
- centerCircle: document.getElementById("circle"),
-    
-    
- init: function() {
-    this.blue.onclick = padClicked;
-    this.red.onclick = padClicked;
-    this.green.onclick = padClicked;
-    this.yellow.onclick = padClicked;
- },
-        
-newMove: function() {
-    var potentialMoves = ["blue", "red", "green", "yellow"];
-    this.moves.push(potentialMoves[Math.floor(Math.random() * 4)]);
-},
+    octave: 3,
+    moves: [],
+    health: 100,
 
-presentMoves: function(){
-    player.currentMove = 0;
-    var i = 0;
-    var that = this;
-    var interval = setInterval(function() {
-        if (i >= that.moves.length) {
-            clearInterval(interval);
-            that.clearPads()
-        }
-        that.clearPads();
+    timeouts: [],
 
-        switch (that.moves[i]) {
-            case "blue":
-                playNote('E', that.octave, 'q');
-                that.blue.className = that.blue.className + " lit";
-                break;
-            case "red":
-                playNote('A', that.octave, 'q');
-                red.className = red.className + " lit";
-                break;
-            case "green":
-                playNote('E', that.octave - 1, 'q');
-                that.green.className = that.green.className + " lit";
-                break;
-            case "yellow":
-                playNote('C#', that.octave, 'q');
-                that.yellow.className = that.yellow.className + " lit";
-                break;
-        }
-        i++;
+    blue: document.getElementById("blue"),
+    red: document.getElementById("red"),
+    green: document.getElementById("green"),
+    yellow: document.getElementById("yellow"),
+    centerCircle: document.getElementById("circle"),
 
-    }, that.health*6);
 
-},
-clearPads: function() {
-    this.blue.className = "tile";
-    this.red.className = "tile";
-    this.green.className = "tile";
-    this.yellow.className = "tile";
-}
+    init: function()
+    {
+        this.blue.onclick = padClicked;
+        this.red.onclick = padClicked;
+        this.green.onclick = padClicked;
+        this.yellow.onclick = padClicked;
+    },
 
-    
+    newMove: function()
+    {
+        var potentialMoves = ["blue", "red", "green", "yellow"];
+        this.moves.push(potentialMoves[Math.floor(Math.random() * 4)]);
+    },
+
+    presentMoves: function()
+    {
+        player.currentMove = 0;
+        var i = 0;
+        var that = this;
+        var interval = setInterval(function()
+        {
+            if (i >= that.moves.length)
+            {
+                clearInterval(interval);
+                that.clearPads()
+            }
+            that.clearPads();
+
+            switch (that.moves[i])
+            {
+                case "blue":
+                    playNote('E', that.octave, 'q');
+                    that.blue.className = that.blue.className + " lit";
+                    break;
+                case "red":
+                    playNote('A', that.octave, 'q');
+                    red.className = red.className + " lit";
+                    break;
+                case "green":
+                    playNote('E', that.octave - 1, 'q');
+                    that.green.className = that.green.className + " lit";
+                    break;
+                case "yellow":
+                    playNote('C#', that.octave, 'q');
+                    that.yellow.className = that.yellow.className + " lit";
+                    break;
+            }
+            i++;
+
+        }, that.health * 6);
+
+    },
+    clearPads: function()
+    {
+        this.blue.className = "tile";
+        this.red.className = "tile";
+        this.green.className = "tile";
+        this.yellow.className = "tile";
+    }
+
+
 }
 
 var player = {
@@ -76,23 +85,26 @@ var player = {
     goingReverse: false,
     lives: 3,
     livesDiv: document.getElementById("lives"),
-    
+
 }
 
-function padClicked(event) {
+function padClicked(event)
+{
     padClicked = event.target.id;
     simon.clearPads();
-    
-    if (simon.moves[player.currentMove] == padClicked || simon.moves[simon.moves.length - player.currentMove - 1] == padClicked) {
+
+    if (simon.moves[player.currentMove] == padClicked || simon.moves[simon.moves.length - player.currentMove - 1] == padClicked)
+    {
         if (simon.moves[simon.moves.length - player.currentMove - 1] == padClicked)
         {
             player.goingReverse = true;
         }
         else
         {
-            player.goingReverse = false;   
+            player.goingReverse = false;
         }
-        switch (padClicked) {
+        switch (padClicked)
+        {
             case "blue":
                 playNote('E', simon.octave, 'q');
                 simon.blue.className = simon.blue.className + " lit";
@@ -112,8 +124,10 @@ function padClicked(event) {
         }
         player.currentMove++;
 
-        if (player.currentMove >= simon.moves.length) {
-            setTimeout(function() {
+        if (player.currentMove >= simon.moves.length)
+        {
+            setTimeout(function()
+            {
                 if (player.goingReverse)
                 {
                     simon.health -= simon.moves.length;
@@ -129,7 +143,9 @@ function padClicked(event) {
 
             }, 100);
         }
-    } else {
+    }
+    else
+    {
         simon.moves = [];
         player.currentMove = 0;
         player.lives -= 1;
@@ -137,13 +153,13 @@ function padClicked(event) {
         playNote("F#", "1", 'q');
         if (player.lives < 0)
         {
-         alert("You Lose!");
-         newGame();
+            alert("You Lose!");
+            newGame();
         }
         else
         {
-        
-            setTimeout(function() {
+            setTimeout(function()
+            {
                 simon.newMove();
                 simon.presentMoves();
 
@@ -153,29 +169,31 @@ function padClicked(event) {
 
 }
 
-function newGame() {
+function newGame()
+{
     simon.moves = [];
     simon.health = 100;
     player.currentMove = 0;
     player.lives = 3;
-    
+
     updateUI();
-    
+
     //
     simon.newMove();
     simon.presentMoves();
 }
 
-function playNote(note, octave, length) {
+function playNote(note, octave, length)
+{
     var sequence = new Sequence(ac, tempo, [note + octave + " " + length]);
     sequence.loop = false;
     sequence.play();
 }
 
-function updateUI(){
+function updateUI()
+{
     player.livesDiv.innerHTML = "Lives: " + player.lives;
     simon.centerCircle.innerHTML = simon.health;
-
 }
 
 simon.init();
